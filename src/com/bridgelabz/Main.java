01bbafbb663d2b066da768a4209be01ce3484def
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 23:23:11 -0700

    fibonacci series

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,8 +1,23 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("welcome to logical problem");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many times you want to print the Fibonacci_Series : ");
+        int times = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b);
+        int c;
+        for (int i = 1; i <= times; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
     }
+
 }
+
